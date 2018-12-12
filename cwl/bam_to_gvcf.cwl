532410641ae79ce546be9367a1cda42d8d1f08cf
Yaobo Xu <yx2@sanger.ac.uk>
2018-12-12 10:33:22 +0000

    completed bam_to_fastq, ready for tests

---
+++ cwl/bam_to_gvcf.cwl
@@ -0,0 +1,127 @@
+#!/usr/bin/env cwl-runner
+
+$namespaces:
+  dct: http://purl.org/dc/terms/
+  foaf: http://xmlns.com/foaf/0.1/
+  s: http://schema.org/
+
+$schemas:
+- http://schema.org/docs/schema_org_rdfa.html
+- http://dublincore.org/2012/06/14/dcterms.rdf
+- http://xmlns.com/foaf/spec/20140114.rdf
+
+
+class: CommandLineTool
+
+id: "correct_bam_and_collect_metrics_for_gatk"
+
+label: "Correct BMA and collect metrics for GATK pipeline"
+
+cwlVersion: v1.0
+
+doc: |
+    ![build_status](https://quay.io/repository/wtsicgp/gatk_eze/status)
+    See the [GATK_Eze](https://github.com/cancerit/GATK_Eze) website for more information.
+
+dct:creator:
+  "@id": "yaobo.xu@sanger.ac.uk"
+  foaf:name: Yaobo Xu
+  foaf:mbox: "yx2@sanger.ac.uk"
+
+requirements:
+  - class: DockerRequirement
+    dockerPull: "quay.io/wtsicgp/gatk_eze:0.0.2"
+
+hints:
+  - class: ResourceRequirement
+    coresMin: 1 # all processes are single threaded
+    ramMin: 8000
+
+inputs:
+
+  in_bam: # Need the index file!!!
+    type: File
+    secondaryFiles:
+    - .bai
+    doc: "input BAM to be converted to GATK ready format and to collect metrics from"
+    inputBinding:
+      position: 1
+      shellQuote: true
+
+  sanger_core_ref:
+    type: File
+    doc: "reference genome files in tar, as used in Sanger, GRCh37d5 can be downloaded here: http://ftp.sanger.ac.uk/pub/cancer/dockstore/human/core_ref_GRCh37d5.tar.gz"
+    inputBinding:
+      position: 2
+      shellQuote: true
+
+  gatk_ref_bundle_gzipped_dbsnp:
+    type: File
+    doc: "gzipped Miils and 1000G golden standard indels vcf file from GATK reference bundle, for Eze's pipeline, please use Mills_and_1000G_gold_standard.indels.b37.vcf.gz. It can be downloaded from ftp://ftp.broadinstitute.org/bundle/, username: gsapubftp-anonymous, no password"
+    inputBinding:
+      position: 3
+      shellQuote: true
+
+  gatk_ref_bundle_gzipped_mills_1000g_gold_indel:
+    type: File
+    doc: "gzipped 1000G phase1 indels vcf file from GATK reference bundle, for Eze's pipeline, please use 1000G_phase1.indels.b37.vcf.gz. It can be downloaded from ftp://ftp.broadinstitute.org/bundle/, username: gsapubftp-anonymous, no password"
+    inputBinding:
+      position: 4
+      shellQuote: true
+
+  gatk_ref_bundle_gzipped_1000g_indel:
+    type: File
+    doc: ""
+    inputBinding:
+      position: 5
+      shellQuote: true
+
+  mem:
+    type: int
+    doc: "number of GBs for max memory of Java processes to use"
+    default: 12
+    inputBinding:
+      position: 6
+
+  cpu:
+    type: int
+    doc: "number of GBs for max memory of Java processes to use"
+    default: 1
+    inputBinding:
+      position: 7
+
+outputs:
+
+  recalibrated_bam:
+    type:
+      type: array
+      items: File
+    outputBinding:
+      glob: $(inputs.in_bam.nameroot).recal.ba*
+
+  g_vcf:
+    type: File
+    outputBinding:
+      glob: $(inputs.in_bam.nameroot).g.vcf.gz
+    secondaryFiles:
+      - .tbi
+
+  bam_recalib_plots:
+    type:
+      type: array
+      items: File
+    outputBinding:
+      glob: "$(inputs.in_bam.nameroot).recal_plots.*"
+
+  job_log:
+    type: File
+    outputBinding:
+      glob: $(inputs.in_bam.nameroot).eze_gatk_part_2_bam2gvcf.log
+
+baseCommand: ["correct_bam_and_get_metrics.sh"]
+
+s:codeRepository: https://github.com/cancerit/GATK_Eze
+s:author:
+  - class: s:Person
+    s:email: mailto:cgphelp@sanger.ac.uk
+    s:name: Yaobo Xu
---
+++ scripts/bam_to_gvcf.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+
+set -xue
+
+########################################################################################################
+# PART 2: From raw basecalls to GATK-ready reads
+########################################################################################################
+
+## Recalibration
+#  - GATK BQSR (Base Quality Score Recalibration) using BaseRecalibrator
+#  - GATK PrintReads
+#  - GATK BQSR (Base Quality Score Recalibration) using BaseRecalibrator
+#  - GATK AnalyzeCovariates
+## Variant Calling
+#  - GATK HaplotypeCaller
+#  - bgzip + tabix .g.vcf
+#  - GATK ValidateVariants
+## File cleanup
+#  - file cleanup
+
+########################################################################################################
+
+in_bam=$1
+# TODO check the index!!!
+sanger_core_ref=$2
+gatk_ref_bundle_gzipped_dbsnp=$3
+gatk_ref_bundle_gzipped_mills_1000g_gold_indel=$4
+gatk_ref_bundle_gzipped_1000g_indel=$5
+mem=$6
+cpu=$7
+
+# untar ref
+mkdir -p ref
+tar xzf $sanger_core_ref -C ref --strip-components 1
+# just because GATK does not think genome.fa.dict is the dict file name!!
+cp ref/genome.fa.dict ref/genome.dict
+
+this_gatk="java -Xmx${mem}g -Djava.io.tmpdir=/tmp -Djava.library.path=/tmp -jar /opt/GenomeAnalysisTK.jar -R ref/genome.fa"
+
+tmp_bam_prefix=${in_bam%.*}
+
+samplelog=${tmp_bam_prefix}.eze_gatk_part_2_bam2gvcf.log
+echo "$(date '+%d/%m/%y_%H:%M:%S'), Wake up to work" > "$samplelog"
+
+echo "$(date '+%d/%m/%y_%H:%M:%S'), preparing GATK bundle files" > "$samplelog"
+gatk_ref_bundle_dbsnp="ref/gatk_ref_bundle_dbsnp.vcf"
+gatk_ref_bundle_mills_1000g_gold_indel="ref/gatk_ref_bundle_mills_1000g_gold_indel.vcf"
+gatk_ref_bundle_1000g_indel="ref/gatk_ref_bundle_1000g_indel.vcf"
+gunzip -c $gatk_ref_bundle_gzipped_dbsnp > $gatk_ref_bundle_dbsnp
+gunzip -c $gatk_ref_bundle_gzipped_mills_1000g_gold_indel > $gatk_ref_bundle_mills_1000g_gold_indel
+gunzip -c $gatk_ref_bundle_gzipped_1000g_indel > $gatk_ref_bundle_1000g_indel
+
+
+########################################################################################################
+# GATK BQSR (Base Quality Score Recalibration) 
+# STEP 1: Creating covariates table masking known indel, SNP sites
+########################################################################################################
+echo "$(date '+%d/%m/%y_%H:%M:%S'),---Starting GATK BaseRecalibrator Pre---" >> "$samplelog"
+
+time ($this_gatk \
+-nct $cpu \
+-T BaseRecalibrator \
+-I $in_bam \
+-knownSites $gatk_ref_bundle_dbsnp \
+-knownSites $gatk_ref_bundle_mills_1000g_gold_indel \
+-knownSites $gatk_ref_bundle_1000g_indel \
+-o ${tmp_bam_prefix}.recal_table) >> "$samplelog"
+
+echo "$(date '+%d/%m/%y_%H:%M:%S'), Finished GATK BaseRecalibrator Pre" >> "$samplelog"
+
+########################################################################################################
+# GATK BQSR (Base Quality Score Recalibration) 
+# STEP 2: Writing the new BAM with recalibrated Q scores
+########################################################################################################
+echo "$(date '+%d/%m/%y_%H:%M:%S'),---Starting GATK PrintReads---" >> "$samplelog"
+
+time ($this_gatk \
+-nct $cpu \
+-T PrintReads \
+-I $in_bam \
+-BQSR ${tmp_bam_prefix}.recal_table \
+-o ${tmp_bam_prefix}.recal.bam) >> "$samplelog"
+
+echo "$(date '+%d/%m/%y_%H:%M:%S'),---GATK PrintReads finished---" >> "$samplelog"
+
+####################################################
+# GATK BQSR post
+# Count covariates in recal.BAM	to compare before/after recalibration
+####################################################
+echo "$(date '+%d/%m/%y_%H:%M:%S'),---Starting GATK BaseRecalibrator Post---" >> "$samplelog"
+
+time ($this_gatk \
+-nct $cpu \
+-T BaseRecalibrator \
+-I $in_bam \
+-knownSites $gatk_ref_bundle_dbsnp \
+-knownSites $gatk_ref_bundle_mills_1000g_gold_indel \
+-knownSites $gatk_ref_bundle_1000g_indel \
+-BQSR ${tmp_bam_prefix}.recal_table \
+-o ${tmp_bam_prefix}.recal_table_post) >> "$samplelog" 
+
+echo "$(date '+%d/%m/%y_%H:%M:%S'), Finished GATK BaseRecalibrator Post" >> "$samplelog"
+
+####################################################
+# GATK AnalyzeCovariates
+# Create a pdf plot to see the results of recalibration
+####################################################
+echo "$(date '+%d/%m/%y_%H:%M:%S'),---Starting GATK AnalyzeCovariates---" >> "$samplelog"
+
+time ($this_gatk \
+-T AnalyzeCovariates \
+-before ${tmp_bam_prefix}.recal_table \
+-after ${tmp_bam_prefix}.recal_table_post \
+-plots ${tmp_bam_prefix}.recal_plots.pdf \
+-csv ${tmp_bam_prefix}.recal_plots.csv \
+-l DEBUG ) >> "$samplelog"
+
+echo "$(date '+%d/%m/%y_%H:%M:%S'),---GATK AnalyzeCovariates finished---" >> "$samplelog"
+
+####################################################
+# Genotyping - HaplotypeCaller
+####################################################
+echo "$(date '+%d/%m/%y_%H:%M:%S'),---Starting HaplotypeCaller---" >> "$samplelog"
+time ($this_gatk \
+-nct $cpu \
+-T HaplotypeCaller \
+-I ${tmp_bam_prefix}.recal.bam \
+--emitRefConfidence GVCF \
+--dbsnp $gatk_ref_bundle_dbsnp \
+-o ${tmp_bam_prefix}.g.vcf.gz \
+-pairHMM VECTOR_LOGLESS_CACHING ) >> "$samplelog"
+
+echo "$(date '+%d/%m/%y_%H:%M:%S'),---Finished HaplotypeCaller---" >> "$samplelog"
+
+####################################################
+# Check the validity of the g.vcf.gz file
+####################################################
+echo "$(date '+%d/%m/%y_%H:%M:%S'),---Starting GVCF validation after HaplotypeCaller---" >> "$samplelog"
+time ($this_gatk \
+-T ValidateVariants \
+-V ${tmp_bam_prefix}.g.vcf.gz \
+--validationTypeToExclude ALL ) >> "$samplelog"
+
+echo "$(date '+%d/%m/%y_%H:%M:%S'),---GVCF validation after HaplotypeCaller COMPLETED---">> "$samplelog"
